4d1078e528efdd8ac4b08ab78e448facc146dad6
Abdur-Rehman <iabdurrehman2.0@gmail.com>
2026-03-28 12:55:46 +0500

    Created a Server...

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,7 +1,10 @@
 import 'dotenv/config';
 import express from 'express';
-
+import cors from 'cors';
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/',(req,res)=>{
   res.send("API is Running! ")
